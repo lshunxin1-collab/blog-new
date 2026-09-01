74aca7638193aa479af545a29f84fa5c7ab2992a
lshunxin1-collab <lshunxin1@gmail.com>
2026-09-01 21:07:15 +0800

    sync UI changes from manager: navbar label, css improvements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -75,7 +75,7 @@ export default function Navbar() {
     { name: '首页', href: '/' },
     { name: '项目', href: '/projects' },
     { name: '归档', href: '/timeline' },
-    { name: '照片墙', href: '/photowall' },
+    { name: '照片', href: '/photowall' },
     { name: '音乐', href: '/music' },
     { name: '说说', href: '/moments' },
     { name: '杂谈', href: '/chatter' },
